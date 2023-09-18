c385a56238cea5f052b4092eef3f6ec904fd73c9
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-18 20:38:28 +0100

    API fetch MoviesList

--- moviesmate/src/components/Card.js
+++ moviesmate/src/components/Card.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-export const Card = () => {
+import Backup  from "./../assets/images/backUp.jpg"
+export const Card = ({movie}) => {
+  const {id, original_title, overview, poster_path} = movie
+  const image = poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : Backup ;
   return (
-    <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
     <Link to="#">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-auto my-4" src={image} alt="" />
     </Link>
-    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    <Link to="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Read more
-        <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-    </Link>
+    <div className="p-5">
+        <Link to="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
+        
+        </div>
+
 </div>
+        
+
+   
 
   )
 }
--- moviesmate/src/pages/MoviesList.js
+++ moviesmate/src/pages/MoviesList.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 import { Card } from "./../components"
+
 export const MoviesList = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(()=> {
+        async function fetchMovies(){
+            const response = await fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=e40d58ef12f677b100318e00a4c4c642");
+            const data = await response.json();
+            setMovies(data.results);
+        }
+    fetchMovies();
+    },[])
   return (
     <main>
         <section className="max-w-7xl mx-auto py-7">
             <div className="flex justify-start flex-wrap">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                { movies.map((movie) => (
+                    <Card key={movie.id} movie={movie}/>
+                ))}
             </div>
         </section>
         </main>
