e6ad315d09c4289321c2d498c40e1178dfe22617
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-18 16:09:26 +0100

    add Mobile Mode to navigation and search bar

--- moviesmate/README.md
+++ moviesmate/README.md
@@ -2,6 +2,9 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Create the React App with the command
+npx create react-app moviesmate
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -23,3 +26,6 @@ npm install react-router-dom@6
 
 ### Install Tailwind css the version 3
 npm install -D tailwindcss@3
+
+### Generate the API Token by creating an account on TMDB
+https://developer.themoviedb.org/
--- moviesmate/src/components/Footer.js
+++ moviesmate/src/components/Footer.js
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom"
 export const Footer = () => {
   return (
 
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            <Link href="/" class="flex items-center mb-4 sm:mb-0">
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">MoviesMate</a>. All Rights Reserved.</span>
+<footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <Link href="/" className="flex items-center mb-4 sm:mb-0">
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">MoviesMate</a>. All Rights Reserved.</span>
             </Link>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Instagram</a>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
                 </li>
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Twitter</a>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">Twitter</a>
                 </li>
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline">Github</a>
+                    <a href="#" className="hover:underline">Github</a>
                 </li>
             </ul>
         </div>
