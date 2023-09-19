0bb30eeee6dadf97edd71030bda76c671ac028ca
mohamedbenaantar <mohamedbenantar@gmail.com>
2023-09-19 12:57:40 +0100

    Add Movie Detail Page

--- moviesmate/src/components/Card.js
+++ moviesmate/src/components/Card.js
@@ -6,11 +6,11 @@ export const Card = ({movie}) => {
   const image = poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : Backup ;
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
-    <Link to="#">
+    <Link to={`movie/${id}`}>
         <img className="object-cover w-full rounded-t-lg h-90  md:rounded-none md:rounded-l-lg" src={image} alt="" />
     </Link>
     <div className="p-5">
-        <Link to="#">
+        <Link to={`movie/${id}`}>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
--- moviesmate/src/routes/AllRoutes.js
+++ moviesmate/src/routes/AllRoutes.js
@@ -8,8 +8,8 @@ export const AllRoutes = () => {
   return (
     <div className="dark:bg-darkbg bg-blue-400">
     <Routes>
-        <Route path="/" element={<MoviesList apiPath="discover/movie"/>}/>
-        <Route path="movie/:id" element={<MovieDetail/>}/>
+        <Route path="/" element={<MoviesList apiPath="movie/now_playing"/>}/>
+        <Route path="movie/:movie_id" element={<MovieDetail/>}/>
         <Route path="movies/popular" element={<MoviesList apiPath="movie/popular"/>}/>
         <Route path="movies/top" element={<MoviesList apiPath="tv/top_rated"/>}/>
         <Route path="movies/upcoming" element={<MoviesList apiPath="movie/upcoming"/>}/>
